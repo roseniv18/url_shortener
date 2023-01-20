1799766aa55df29f4d19f2f669d2b937d1e2be44
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-01-20 15:58:55 +0200

    fixed mobile layout

--- src/Components/URLContainer.js
+++ src/Components/URLContainer.js
@@ -5,14 +5,14 @@ function URLContainer({ short_link, long_link }) {
   const [copied, setCopied] = useState({
     state: true,
     text: "Copy",
-    styles: "px-6 py-2 text-white bg-teal-500"
+    styles: "bg-teal-500"
   })
   const copyToClipboard = () => {
     setCopied({
         ...copied,
         state: !copied.state,
         text: copied.state ? "Copied" : "Copy",
-        styles: copied.state ? "px-6 py-2 text-white bg-violet-900" : "px-6 py-2 text-white bg-teal-500"
+        styles: copied.state ? "bg-violet-900" : "bg-teal-500"
     })
     if(copied.state) {
       return navigator.clipboard.writeText(short_link)
@@ -23,11 +23,11 @@ function URLContainer({ short_link, long_link }) {
 
   return (
     <div className="w-full">
-      <div className="flex flex-row md:w-full lg:w-full justify-between items-center bg-white lg:px-12 md:px-16 sm:px-6 py-2 mt-5 gap-5 relative rounded-md">
-        <a href={long_link} target="_blank" noreferrer>{long_link}</a>
-        <div className="flex flex-row justify-between items-center md:space-x-4 sm:space-x-2">
-          <a href={long_link} target="_blank" noreferrer>{short_link}</a>
-          <button className = {copied.styles} onClick={copyToClipboard}>{copied.text}</button>
+      <div className="flex md:flex-row sm:flex-col md:w-full lg:w-full justify-between md:items-center bg-white lg:px-12 md:px-16 sm:px-6 py-2 mt-5 gap-5 relative rounded-md">
+        <a href={long_link} target="_blank" rel="noreferrer">{long_link}</a>
+        <div className="flex md:flex-row md:items-center sm:flex-col md:space-x-4 sm:space-y-2 sm:my-2">
+          <a href={long_link} target="_blank" rel="noreferrer" className="text-teal-500">{short_link}</a>
+          <button className = {`rounded-md px-6 py-2 text-white ${copied.styles}`} onClick={copyToClipboard}>{copied.text}</button>
         </div>
       </div>
     </div>
